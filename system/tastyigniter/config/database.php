cd0975b01d0f7ce61dc39f4e28b8a28ac32843ba
Juan <jjrosalesuci@gmail.com>
2017-06-28 11:51:17 -0300

    add database file

---
+++ system/tastyigniter/config/database.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default']['dsn'] = '';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'bicasoft';
+$db['default']['database'] = 'foodridersdbnew6';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = 'c9mc0lrpq_';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['encrypt']  = FALSE;
+$db['default']['compress'] = FALSE;
+$db['default']['stricton'] = FALSE;
+$db['default']['failover'] = array();
+$db['default']['save_queries'] = TRUE;
+
+/* End of file database.php */
+/* Location: ./system/tastyigniter/config/database.php */
